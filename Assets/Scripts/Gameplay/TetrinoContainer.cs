1e1175945d6e34357aca8a122fc9ed2d4da96a79
Schweppie <joeppetersmail@gmail.com>
2019-07-10 11:24:51 +0200

    Add tetrino spawner and container

---
+++ Assets/Scripts/Gameplay/TetrinoContainer.cs
@@ -0,0 +1,40 @@
+﻿using JP.Mytris.Data;
+
+namespace JP.Mytrix.Gameplay
+{
+    public class TetrinoContainer : DisposableData
+    {
+        private int position;
+
+        private TetrinoConfig config;
+        public TetrinoConfig Config => config;
+
+        public delegate void PositionUpdateDelegate(int position);
+        public event PositionUpdateDelegate OnPositionUpdateEvent;
+
+        public TetrinoContainer (int position, TetrinoConfig config)
+        {
+            this.position = position;
+            this.config = config;
+        }
+
+        public void MoveUp()
+        {
+            position--;
+
+            if (position < 0)
+            {
+                Dispose();
+                return;
+            }
+
+            DispatchOnPositionUpdateEvent();
+        }
+
+        private void DispatchOnPositionUpdateEvent()
+        {
+            if (OnPositionUpdateEvent != null)
+                OnPositionUpdateEvent(position);
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/TetrinoSpawner.cs
@@ -0,0 +1,55 @@
+﻿using JP.Mytris.Data;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JP.Mytrix.Gameplay
+{
+    public class TetrinoSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private TetrinoConfig[] configs;
+
+        private List<TetrinoContainer> tetrinoContainters = new List<TetrinoContainer>();
+
+        private const int STACK_SIZE = 3;
+
+        public delegate void TetrinoSpawnedDelegate(TetrinoConfig config);
+        public event TetrinoSpawnedDelegate OnTetrinoSpawnedEvent;
+
+        public void Setup()
+        {
+            tetrinoContainters.Clear();
+
+            for (int i = 0; i < STACK_SIZE; i++)
+            {
+                TetrinoContainer tetrinoContainer = new TetrinoContainer(i, configs[Random.Range(0, configs.Length)]);
+                tetrinoContainters.Add(tetrinoContainer);
+            }
+        }
+
+        public void SpawnTetrino()
+        {
+            if (tetrinoContainters.Count == 0)
+                return;
+
+            TetrinoContainer tetrinoContainer = tetrinoContainters[0];
+            tetrinoContainer.Dispose();
+
+            tetrinoContainters.RemoveAt(0);
+
+            for (int i = 0; i < tetrinoContainters.Count; i++)
+                tetrinoContainters[i].MoveUp();
+
+            TetrinoContainer newTetrinoContainer = new TetrinoContainer(tetrinoContainters.Count - 1, configs[Random.Range(0, configs.Length)]);
+            tetrinoContainters.Add(newTetrinoContainer);
+
+            DispatchOnTetrinoSpawnedEvent(tetrinoContainer.Config);
+        }
+
+        private void DispatchOnTetrinoSpawnedEvent(TetrinoConfig config)
+        {
+            if (OnTetrinoSpawnedEvent != null)
+                OnTetrinoSpawnedEvent(config);
+        }
+    }
+}
